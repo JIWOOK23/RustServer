658e696d3658cf5cf257fb31264e040f37a4f353
YangJiwook <didwldnr223@gmail.com>
2024-08-01 18:39:14 +0900

    http 라이브러리 생성 및 테스트

--- http/src/HttpResponse.rs
+++ http/src/HttpResponse.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::io::{Result, Write};
 
 #[derive(Debug,PartialEq,Clone)]
-pub struct Httpresponse<'a>{
+pub struct HttpResponse<'a>{
     version : &'a str,
     status_code: &'a str,
     status_text: &'a str,
@@ -10,7 +10,7 @@ pub struct Httpresponse<'a>{
     body: Option<String>,
 }
 
-impl<'a> Default for Httpresponse<'a>{
+impl<'a> Default for HttpResponse<'a>{
     fn default() -> Self {
         Self {
             version: "HTTP/1.1".into(),
@@ -22,13 +22,13 @@ impl<'a> Default for Httpresponse<'a>{
     }
 }
 
-impl<'a> Httpresponse<'a>{
+impl<'a> HttpResponse<'a>{
     pub fn new(
         status_code: &'a str,
         headers: Option<HashMap<&'a str,&'a str>>,
         body: Option<String>,
-    ) -> Httpresponse<'a>{
-        let mut response: Httpresponse<'a> = Httpresponse::default();
+    ) -> HttpResponse<'a>{
+        let mut response: HttpResponse<'a> = HttpResponse::default();
         if status_code != "200"{
             response.status_code = status_code.into();
         };
@@ -87,8 +87,8 @@ impl<'a> Httpresponse<'a>{
     }
 }
 
-impl<'a> From<Httpresponse<'a>> for String {
-    fn from(res: Httpresponse) -> String {
+impl<'a> From<HttpResponse<'a>> for String {
+    fn from(res: HttpResponse) -> String {
         let res1 = res.clone();
         format!("{} {} {}\r\n{}Content-Length: {}\r\n\r\n{}",
         &res1.clone().version(),
@@ -109,12 +109,12 @@ mod tests{
     //테스트 케이스 에는 #[test] 애너테이션을 붙여야 한다.
     #[test]
     fn test_response_struct_creation_200(){
-        let response_actual = Httpresponse::new(
+        let response_actual = HttpResponse::new(
             "200",
             None,
             Some("Item was shipped on 21st Dec 2020".into()),
         );
-        let response_expected = Httpresponse{
+        let response_expected = HttpResponse {
             version: "HTTP/1.1",
             status_code: "200",
             status_text: "OK",
@@ -130,12 +130,12 @@ mod tests{
 
     #[test]
     fn test_response_struct_creation_404(){
-        let response_actual = Httpresponse::new(
+        let response_actual = HttpResponse::new(
             "404",
             None,
             Some("Item was shipped on 21st Dec 2020".into()),
         );
-        let response_expected = Httpresponse{
+        let response_expected = HttpResponse {
             version: "HTTP/1.1",
             status_code:  "404",
             status_text: "Not Found",
@@ -151,7 +151,7 @@ mod tests{
 
     #[test]
     fn test_http_response_creation(){
-        let response_expected = Httpresponse{
+        let response_expected = HttpResponse {
             version: "HTTP/1.1",
             status_code: "404",
             status_text: "Not Found",
---
+++ httpserver/src/server.rs
@@ -0,0 +1,28 @@
+use super::router::Router;
+use http::httprequest::HttpRequest;
+use std::io::prelude::*;
+use std::net::{SocketAddr, TcpListener};
+use std::str;
+
+pub struct Server<'a>{
+    socket_addr: &'a str,
+}
+
+impl<'a> Server<'a> {
+    pub fn new(socket_addr: &'a str) -> Self{
+        Server { socket_addr}
+    }
+
+    pub fn run(&self){
+        let connection_listner = TcpListener::bind(self.socket_addr).unwrap();
+        print!("Running on {}", self.socket_addr);
+        for stream in connection_listner.incoming(){
+            let mut stream = stream.unwrap();
+            print!("Connection established");
+            let mut read_buffer = [0; 90];
+            stream.read(&mut read_buffer).unwrap();
+            let req: HttpRequest = String::from_utf8(read_buffer.to_vec()).unwrap().into();
+            Router::route(req,&mut stream);
+        }
+    }
+}
